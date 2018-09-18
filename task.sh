3ffa3e8011e70f3bb7b7a7ab8d3539410106b88a
kkapoor <kunal18kapoor@yahoo.com>
2018-09-18 13:27:14 +0000

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,8 +1,8 @@
 REGION=us-east-1
-
-
-
-
-
-
-file://sample-v_${BUILD_NUMBER}.json
+REPOSITORY_NAME=myrepo
+CLUSTER=mycluster
+FAMILY=spring3hibernate
+NAME=spring3hibernate
+SERVICE_NAME=spring3hibernatespring3hibernate-service
+sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" /home/ec2-user/tasktemplate.json > sample-v_${BUILD_NUMBER}.json
+aws ecs register-task-definition --region ${REGION} --family ${FAMILY} --cli-input-json file://sample-v_${BUILD_NUMBER}.json
